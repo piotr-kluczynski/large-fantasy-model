9eba7136935ef8d5193a37638ed026c113d9d72b
Piotr Kluczynski <peklu@wp.pl>
2026-03-09 20:49:41 +0100

    Added the initial version of the empty ASP .NET MVC project.

---
+++ large-fantasy-model/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace large_fantasy_model.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
